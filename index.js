529a0204f9e9e1ff8263e76b4d369ddfa0366d62
unknown <kitey13579@gmail.com>
2021-03-28 16:16:09 +0100

    v1.1.0 - Systray functionality w/ electron

--- index.js
+++ index.js
@@ -3,52 +3,89 @@ const {
     cookie,
     userId
 } = require("./config.json")
-
+const path = require("path")
+const {app, Menu, Tray} = require("electron")
 const { getPlaceId } = require("./utils/getPlaceId")
 const { setPresence } = require("./utils/setPresence")
 const rpc = require("discord-rpc")
 const noblox = require("noblox.js")
+
+const iconPath = path.join(__dirname, "logo.png")
+let tray = null
+let isPlaying = false
+let lastId;
+
 const client = new rpc.Client({
     transport: "ipc"
 })
 
-let isPlaying = false
-let lastId;
 
+async function updateTrayMenu() {
+    const robloxUsername = await noblox.getUsernameFromId(userId) || "Unknown"
+    let template = [{
+            label: `ROBLOX Username: ${robloxUsername}`,
+            type: "normal",
 
-client.on("ready", async () => {
-    console.log("Ready.")
-    await noblox.setCookie(cookie)
+        },
+        {
+            label: `Discord ${client.user.username}#${client.user.discriminator}`,
+            type: "normal"
+        },
+        {
+            label: "Quit",
+            click: async () => {
+                await client.destroy()
+                app.quit()
+            }
+        }
+    ]
+    const ctxMenu = Menu.buildFromTemplate(template)
+    return ctxMenu
+}
+
+app.whenReady().then(async () => {
+    console.log("Electron Ready")
     
-    setInterval(async () => {
-        const placeId = await getPlaceId(userId)
-        if (placeId === -1) {
-            console.log("Not in a game, clearing presence.")
-            await client.clearActivity()
-            isPlaying = false
-        } 
+    client.on("ready", async () => {
+        console.log("RPC Ready")
 
-        else if (!isPlaying && placeId !== -1 || lastId !== placeId) {
-            await setPresence(client, placeId).catch(err => console.error(err))
-            console.log("Updated presence")
-            lastId = placeId
-            isPlaying = true
-        }
-        else {
-            console.log("In a game")
-        }
-    }, 15e3)
+        tray = new Tray(iconPath)
+        const ctxMenu = await updateTrayMenu()
+        tray.setContextMenu(ctxMenu)
+
+        tray.setToolTip("ROBLOX Discord Rich Presence")
+        await noblox.setCookie(cookie)
+        
+        setInterval(async () => {
+            const placeId = await getPlaceId(userId)
+            
+            if (placeId === -1) {
+                console.log("Not in a game, clearing presence.")
+                await client.clearActivity()
+                isPlaying = false
+            } 
+    
+            else if (!isPlaying && placeId !== -1 || lastId !== placeId) {
+                await setPresence(client, placeId).catch(err => console.error(err))
+                console.log("Updated presence")
+                lastId = placeId
+                isPlaying = true
+            }
+            else {
+                console.log("In a game")
+            }
+        }, 15e3)
+    })
 })
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
 
 client.login({
     clientId: clientId
 })
 
-
-/*  
-
-https://www.npmjs.com/package/systray i might want this
-https://www.npmjs.com/package/systray2 or this
-OR ELECTRON.JS MIGHT BE THE BEST OPTION FOR MINIMIZING TO SYSTEM TRAY
-
-*/
