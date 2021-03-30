a177c96edfd7dfdc98d5d2de37dc2df61a9bf33d
unknown <kitey13579@gmail.com>
2021-03-30 17:50:12 +0100

    v1.3.0 - Notifications added to systray menu, RoVer implementation

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
-const {
-    clientId,
-    cookie,
-    userId
-} = require("./config.json")
-const path = require("path")
-const {app, Menu, Tray} = require("electron")
+const { clientId, cookie } = require("./config.json")
+const { app, Menu, Tray, Notification } = require("electron")
 const { getPlaceId } = require("./utils/getPlaceId")
+const path = require("path")
+const { findRobloxInfo } = require("./utils/findRobloxInfo")
 const { setPresence } = require("./utils/setPresence")
 const rpc = require("discord-rpc")
 const noblox = require("noblox.js")
 
-const iconPath = path.join(__dirname, "logo.png")
+
 let tray = null
 let isPlaying = false
 let lastId;
+let robloxId;
+let notInGameCount = 0
+const iconPath = path.join(__dirname, "logo.png")
+
 
 const client = new rpc.Client({
     transport: "ipc"
 })
 
+async function initData() {
+    const discordTag = client.user.username + "#" + client.user.discriminator
+    const discordId = client.user.id
+    const data = await findRobloxInfo(discordId)
+    if (!data) {
+        new Notification({
+        title: "Not verified with RoVer!", 
+        body: `Your Discord account (${discordTag}) is not verified with RoVer, please verify to use roblox-rpc.`,
+        icon: iconPath
+        }).show()
 
-async function updateTrayMenu() {
-    const robloxUsername = await noblox.getUsernameFromId(userId) || "Unknown"
+        await client.destroy()
+        app.quit()
+        process.exit(1)
+    }
+
+    console.log("Found RoVer details", data)
+    const robloxUsername = data.robloxUsername
+    robloxId = data.robloxId
+
+
     let template = [{
-            label: `ROBLOX Username: ${robloxUsername}`,
+            label: `Verified ✅ : ${robloxUsername}`,
             type: "normal",
-
+            click: async () => {
+                new Notification({
+                    title: "Verified with Rover ✅",
+                    body: `Account: ${robloxUsername}`, 
+                    closeButtonText: "Close button",
+                    icon: iconPath
+                }).show()
+            }
         },
         {
-            label: `Discord ${client.user.username}#${client.user.discriminator}`,
-            type: "normal"
+            label: `Discord Tag : ${discordTag}`,
+            type: "normal",
+            click: async () => {
+                new Notification({
+                    title: "Discord Info",
+                    body: `Discord Tag: ${discordTag}\nDiscord ID: ${discordId}`, 
+                    closeButtonText: "Close button",
+                    icon: iconPath
+                }).show()
+            }
         },
         {
             label: "Quit",
@@ -43,40 +77,65 @@ async function updateTrayMenu() {
     return ctxMenu
 }
 
+
 app.whenReady().then(async () => {
     console.log("Electron Ready")
-    
+
     client.on("ready", async () => {
         console.log("RPC Ready")
-
-        tray = new Tray(iconPath)
-        const ctxMenu = await updateTrayMenu()
-        tray.setContextMenu(ctxMenu)
-
-        tray.setToolTip("ROBLOX Discord Rich Presence")
-        await noblox.setCookie(cookie)
         
+        tray = new Tray(iconPath)
+        tray.setToolTip("ROBLOX Discord Rich Presence")
+
+        const ctxMenu = await initData()
+        tray.setContextMenu(ctxMenu)
+        await noblox.setCookie(cookie)
+
+        // RPC UPDATING
         setInterval(async () => {
-            const placeId = await getPlaceId(userId)
-            
-            if (placeId === -1) {
+            const placeId = await getPlaceId(robloxId)
+            if (notInGameCount >= 960) {
+                new Notification({
+                    title: "Not playing ROBLOX.",
+                    body: "You have not played any ROBLOX game in the past 4 hours, roblox-rpc will automatically close in 10 seconds",
+                    closeButtonText: "Close button",
+                    timeoutType: "never",
+                    icon: iconPath
+                }).show()
+                
+                await new Promise(r => setTimeout(r, 10e3));
+                await client.destroy()
+                app.quit()
+                process.exit(1)
+            }
+
+            else if (placeId === -1) {
                 console.log("Not in a game, clearing presence.")
+                notInGameCount += 1
                 await client.clearActivity()
                 isPlaying = false
-            } 
-    
+            }
+            
             else if (!isPlaying && placeId !== -1 || lastId !== placeId) {
+                console.log(placeId)
                 await setPresence(client, placeId).catch(err => console.error(err))
                 console.log("Updated presence")
+
+                notInGameCount = 0
                 lastId = placeId
                 isPlaying = true
             }
+            
             else {
                 console.log("In a game")
             }
         }, 15e3)
     })
 })
+
+if (process.platform === "win32") {
+   app.setAppUserModelId(app.name);
+}
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -85,7 +144,12 @@ app.on('window-all-closed', () => {
 });
 
 
-client.login({
-    clientId: clientId
+client.login({clientId}).catch(err => {
+    new Notification({
+        title: "Could not login", 
+        body: err.message + "\nPlease restart Discord and try again if this persists.", 
+        icon: iconPath
+    }).show()
 })
 
+// USE electron-builder instead of electron-packager possibly for auto updates stuff etcs and a better exe
