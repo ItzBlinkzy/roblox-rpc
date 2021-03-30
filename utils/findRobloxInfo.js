036d90bc971c7a4f5ba122b1bcbadb0c50498b45
unknown <kitey13579@gmail.com>
2021-03-30 17:45:46 +0100

    v1.2.0 - Dynamically get roblox information w/ RoVer

---
+++ utils/findRobloxInfo.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch")
+async function findRobloxInfo(discordId) {
+    const url = `https://verify.eryn.io/api/user/${discordId}`
+    const response = await fetch(url)
+    const data = await response.json()
+    
+    if (data.errorCode === 404) {
+        return false
+    }
+
+    return data
+}
+
+module.exports.findRobloxInfo = findRobloxInfo
