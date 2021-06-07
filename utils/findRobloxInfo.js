62e51abb83a43969fe19aea03b24127841d2464a
unknown <kitey13579@gmail.com>
2021-06-07 23:11:50 +0100

    v1.3.14.1 - JSDocs in function

--- utils/findRobloxInfo.js
+++ utils/findRobloxInfo.js
@@ -1,4 +1,11 @@
 const fetch = require("node-fetch")
+
+/**
+ * 
+ * @param {String} discordId 
+ * @returns {boolean | object}
+ */
+
 async function findRobloxInfo(discordId) {
     const url = `https://verify.eryn.io/api/user/${discordId}`
     const response = await fetch(url)
